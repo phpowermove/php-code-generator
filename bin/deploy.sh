caaee716a4d56c03b7ea9660eb2d00273843e3d9
gossi <github@gos.si>
2015-06-18 18:41:19 +0200

    Let's try auto API

---
+++ bin/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+mkdir _site
+cp -R api _site/
+cd _site
+
+git init
+git config user.name "gossi"
+git config user.email "github@gos.si"
+
+git remote add upstream "https://$GH_TOKEN@github.com/gossi/php-code-generator.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
