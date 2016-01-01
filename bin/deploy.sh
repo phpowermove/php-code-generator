0bbe5681cd6e258373d07aaf6beddde1e9f02a84
gossi <github@gos.si>
2016-01-01 17:18:40 +0100

    Go travis, tell me!

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,12 +13,18 @@ git init
 git config user.name "gossi"
 git config user.email "github@gos.si"
 
+echo "Add upstream:"
 git remote add upstream "https://$GH_TOKEN@github.com/gossi/php-code-generator.git"
+echo "Fetch upstream:"
 git fetch upstream
+echo "Reset to gh-pages:"
 git reset upstream/gh-pages
 
 touch .
 
+echo "Add everything new:"
 git add -A .
+echo "Commit:"
 git commit -m "rebuild pages at ${rev}"
+echo "Push:"
 git push upstream HEAD:gh-pages
