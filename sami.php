f2af6e9088cae1c9fda8eef46f95e4bbc449f7c1
gossi <github@gos.si>
2016-01-01 19:14:09 +0100

    About to generate versioned API

--- sami.php
+++ sami.php
@@ -2,13 +2,25 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Sami\Sami;
+use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
 
-$iterator = Finder::create()->files()->name('*.php')->in(__DIR__ . '/src');
+$dir = __DIR__ . '/src';
+$iterator = Finder::create()
+	->files()
+	->name('*.php')
+	->in($dir);
+
+$versions = GitVersionCollection::create($dir)
+    ->addFromTags('v0.*')
+    ->add('master', 'master branch')
+;
 
 return new Sami($iterator, [
 	'title' => 'PHP Code Generator API',
 	'theme' => 'default',
-	'build_dir' => __DIR__ . '/api',
+	'versions' => $versions,
+	'build_dir' => __DIR__ . '/api/%version%',
+	'cache_dir' => __DIR__ . '/cache/%version%',
 	'default_opened_level' => 2
 ]);
