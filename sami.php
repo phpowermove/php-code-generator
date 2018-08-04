8728b5f37b6e0cba610950bf6a2bf8af720b480d
gossi <gossi@users.noreply.github.com>
2018-08-04 21:21:11 +0200

    update travis and sami

--- sami.php
+++ sami.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Sami\Sami;
 use Sami\Version\GitVersionCollection;
 use Symfony\Component\Finder\Finder;
+use Sami\Parser\Filter\PublicFilter;
 
 $dir = __DIR__ . '/src';
 $iterator = Finder::create()
@@ -17,11 +18,22 @@ $versions = GitVersionCollection::create($dir)
 	->add('master', 'master branch')
 ;
 
-return new Sami($iterator, [
+$sami = new Sami($iterator, [
 	'title' => 'PHP Code Generator API',
 	'theme' => 'default',
 	'versions' => $versions,
 	'build_dir' => __DIR__ . '/api/%version%',
 	'cache_dir' => __DIR__ . '/cache/%version%',
-	'default_opened_level' => 2
+	'default_opened_level' => 2,
+	'sort_class_properties' => true,
+	'sort_class_methods' => true,
+	'sort_class_constants' => true,
+	'sort_class_traits' => true,
+	'sort_class_interfaces' => true
 ]);
+
+$sami['filter'] = function () {
+	return new PublicFilter();
+};
+
+return $sami;
